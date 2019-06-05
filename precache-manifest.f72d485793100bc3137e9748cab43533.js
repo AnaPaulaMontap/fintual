4113609c9aa049de1cdaaa0881862820396e0d61
AnaPaulaMOntap <anapaula.monsalvet@gmail.com>
2019-06-05 01:29:55 -0400

    Updates

---
+++ precache-manifest.f72d485793100bc3137e9748cab43533.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab76b79615475d9d61465f1ed462bc81",
+    "url": "/fintual/index.html"
+  },
+  {
+    "revision": "597f97cf3f84cb3c6917",
+    "url": "/fintual/static/css/main.5926b051.chunk.css"
+  },
+  {
+    "revision": "8858af08f4f1dfa20da1",
+    "url": "/fintual/static/js/2.fe53e9f2.chunk.js"
+  },
+  {
+    "revision": "597f97cf3f84cb3c6917",
+    "url": "/fintual/static/js/main.df566f3c.chunk.js"
+  },
+  {
+    "revision": "5350c60d0a1bfcf0fcb0",
+    "url": "/fintual/static/js/runtime~main.250269ab.js"
+  },
+  {
+    "revision": "4144e8008d4cd0980221bd3ccc35a271",
+    "url": "/fintual/static/media/Challenge(1).4144e800.png"
+  }
+]);
